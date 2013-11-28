491913aea04e4fa7b07df9b8f3bca2e4640f751c
meza <meza@meza.hu>
2013-11-28 01:19:59 +0000

    usernames instead of ids

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,23 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+
+  def user_path(user, options={})
+    user_url(user, options.merge(:only_path => true))
+  end
+
+  def edit_user_path(user, options={})
+    edit_user_url(user, options.merge(:only_path => true))
+  end
+
+  def user_url(user, options={})
+    url_for(options.merge(:controller => 'users', :action => 'show',
+                          :username => user.username))
+  end
+
+  def edit_user_url(user, options={})
+    url_for(options.merge(:controller => 'users', :action => 'edit',
+                          :username => user.username))
+  end
+
 end
